b871879a4be0df2e53c149d6b0974ed475a18864
dev1week <rondo2860@gmail.com>
2023-11-09 16:58:57 +0900

    hotifx: properties 수정

--- src/main/java/com/dang/dnolja/auth/config/SecurityConfig.java
+++ src/main/java/com/dang/dnolja/auth/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -45,5 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/*", "/verify/*").permitAll()
                 .antMatchers("/authCheck", "/recommendation/*").authenticated();
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/webjars/**", "/images/**", "/css/**", "/h2-console/**");
     }
 }
