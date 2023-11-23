c44ba325e7b2732a027d09d6e10103b74d8004f8
dev1week <rondo2860@gmail.com>
2023-11-24 00:51:08 +0900

    fix: 이메일 인증 링크 변경(#53)

--- src/main/java/com/dang/dnolja/email/controller/EmailVerificationController.java
+++ src/main/java/com/dang/dnolja/email/controller/EmailVerificationController.java
@@ -1,17 +1,17 @@
 package com.dang.dnolja.email.controller;
 
 import com.dang.dnolja.email.model.service.EmailVerificationService;
-import com.dang.dnolja.global.response.CommonResponse;
 import com.dang.dnolja.user.model.dto.UserDto;
 import com.dang.dnolja.user.model.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@RestController
+@Controller
 @AllArgsConstructor
 public class EmailVerificationController {
 
@@ -20,8 +20,7 @@ public class EmailVerificationController {
 
 
     @GetMapping("/verify/email")
-    public CommonResponse verifyEmail(@RequestParam String id){
-
+    public String verifyEmail(@RequestParam String id){
 
         log.debug("[EmailVerificationController verifyEmail] id :: {}", id);
         //id값을 이용하여 email을 조회한다.
@@ -35,11 +34,12 @@ public class EmailVerificationController {
             log.debug("[EmailVerificationController verifyEmail] user ::{}",user);
             userService.updateVerified(user.getId());
 
-            return new CommonResponse<String>(user.getUsername()+"님의 이메일 인증이 완료되었습니다.");
+            return "redirect:https://dhflxhdxhd.github.io";
         }
 
         throw new IllegalArgumentException("이메일 인증 url이 잘못되었습니다.");
     }
+
 
 
 }
